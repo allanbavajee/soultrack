ae61c63ddfedfa8ab02f45cfe945dc1888d58645
allanbavajee <allan.bavajee@gmail.com>
2025-09-23 16:33:52 +0400

    Update add-member.js

--- pages/add-member.js
+++ pages/add-member.js
@@ -179,6 +179,7 @@ export default function AddMember() {
                 prenom: "",
                 telephone: "",
                 email: "",
+                ville: "",
                 statut: "nouveau",
                 how_came: "",
                 besoin: "",
