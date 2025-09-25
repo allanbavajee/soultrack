a30ae924e0513c58005225d275eff8deda9583d3
allanbavajee <allan.bavajee@gmail.com>
2025-09-25 11:04:44 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
       {/* Logos + slogan */}
       <div className="flex flex-col items-center mt-12 mb-8">
         <div className="flex space-x-6 mb-4">
-          <Image src="/logo1.png" alt="Logo 1" width={90} height={90} />
-          <Image src="/logo2.png" alt="Logo 2" width={90} height={90} />
+          <Image src="/soul.logo.png" alt="Logo 1" width={90} height={90} />
+          <Image src="/icc.logo.png" alt="Logo 2" width={90} height={90} />
         </div>
         <h2 className="text-2xl md:text-3xl font-handwriting text-center text-indigo-700">
           ✨ Tu es précieux, tu es attendu, tu es aimé ✨
