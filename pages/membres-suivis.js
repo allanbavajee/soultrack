a70ba4d8d660bdbfd5b6077ce7e68ee8880180cd
allanbavajee <allan.bavajee@gmail.com>
2025-09-29 12:45:57 +0400

    Update membres-suivis.js

--- pages/membres-suivis.js
+++ pages/membres-suivis.js
@@ -1,1 +1,21 @@
-readme.html
+ {/* Ajouter un membre */}
+        <Link
+          href="/add-member"
+          className="bg-white p-6 w-64 h-52 rounded-3xl shadow-lg flex flex-col items-center justify-center hover:shadow-2xl transition-all duration-200 border-t-4 border-[#4285F4]"
+        >
+          <div className="text-5xl mb-4">➕</div>
+          <h2 className="text-xl font-bold text-gray-800 text-center">
+            Ajouter un membre
+          </h2>
+        </Link>        
+
+        {/* Liste des membres */}
+        <Link
+          href="/list-members"
+          className="bg-white p-6 w-64 h-52 rounded-3xl shadow-lg flex flex-col items-center justify-center hover:shadow-2xl transition-all duration-200 border-t-4 border-[#34a853]"
+        >
+          <div className="text-5xl mb-4">👥</div>
+          <h2 className="text-xl font-bold text-gray-800 text-center">
+            Liste des membres
+          </h2>
+        </Link>
