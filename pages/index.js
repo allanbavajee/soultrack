989f483299d5f84ddf5894a3182e7d4f25a938e2
allanbavajee <allan.bavajee@gmail.com>
2025-09-29 12:45:12 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,26 +18,14 @@ export default function Home() {
 
       {/* Cartes d'actions */}
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6 w-full max-w-5xl mt-10 justify-items-center">
-        {/* Ajouter un membre */}
+         
+        {/* membres-suivi */}
         <Link
-          href="/add-member"
-          className="bg-white p-6 w-64 h-52 rounded-3xl shadow-lg flex flex-col items-center justify-center hover:shadow-2xl transition-all duration-200 border-t-4 border-[#4285F4]"
+          href="/membres-suivis"
+          className="bg-white p-6 rounded-3xl shadow-lg flex flex-col items-center hover:shadow-2xl transition-all duration-200 border-t-4 border-[#9c27b0]"
         >
-          <div className="text-5xl mb-4">➕</div>
-          <h2 className="text-xl font-bold text-gray-800 text-center">
-            Ajouter un membre
-          </h2>
-        </Link>
-
-        {/* Liste des membres */}
-        <Link
-          href="/list-members"
-          className="bg-white p-6 w-64 h-52 rounded-3xl shadow-lg flex flex-col items-center justify-center hover:shadow-2xl transition-all duration-200 border-t-4 border-[#34a853]"
-        >
-          <div className="text-5xl mb-4">👥</div>
-          <h2 className="text-xl font-bold text-gray-800 text-center">
-            Liste des membres
-          </h2>
+          <div className="text-5xl mb-4">👤</div>
+          <h2 className="text-xl font-bold text-gray-800 text-center">Membres & Suivis</h2>
         </Link>
 
         {/* Page évangélisation */}
