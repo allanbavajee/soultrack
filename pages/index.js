588ba9e225addc63e58646807d7403e504f2055e
allanbavajee <allan.bavajee@gmail.com>
2025-10-03 09:38:36 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function LoginPage() {
           <img
             src="/logo.png"
             alt="Logo SoulTrack"
-            className="w-15 h-15 object-contain"
+            className="w-12.5 h-12.5 object-contain"
           />
           SoulTrack
         </h1>
