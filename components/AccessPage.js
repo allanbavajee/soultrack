09d773953f6367d29dd726aaaf3dbabecb1f09dd
allanbavajee <allan.bavajee@gmail.com>
2025-10-14 18:12:26 +0400

    Update AccessPage.js

--- components/AccessPage.js
+++ components/AccessPage.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import supabase from "../../lib/supabaseClient";
+import supabase from "../lib/supabaseClient";
 
 export default function AccessPage() {
   const router = useRouter();
