c7703d654f735957f8eca10bb9a94e1092032d32
allanbavajee <allan.bavajee@gmail.com>
2025-10-22 16:46:43 +0400

    Update membres-cellule.js

--- pages/admin/membres-cellule.js
+++ pages/admin/membres-cellule.js
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import supabase from "../lib/supabaseClient";
+import supabase from "../../lib/supabaseClient";
 
 export default function MembresCellule() {
   const [membres, setMembres] = useState([]);
