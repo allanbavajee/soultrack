014bfbcd10554c5ecd668abc79b879ef905724bf
allanbavajee <allan.bavajee@gmail.com>
2025-10-24 08:15:06 +0400

    Update create-internal-user.js

--- pages/admin/create-internal-user.js
+++ pages/admin/create-internal-user.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/router";
-import AccessGuard from "../components/AccessGuard";
+import AccessGuard from "../../components/AccessGuard";
 
 export default function CreateResponsable() {
   const router = useRouter();
