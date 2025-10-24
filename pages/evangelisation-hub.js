42249718a3ba911e2d8f722b2b354ca2a370a63c
allanbavajee <allan.bavajee@gmail.com>
2025-10-24 08:10:28 +0400

    Update evangelisation-hub.js

--- pages/evangelisation-hub.js
+++ pages/evangelisation-hub.js
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import SendLinkPopup from "../components/SendLinkPopup";
 import LogoutLink from "../components/LogoutLink";
+import AccessGuard from "../components/AccessGuard";
 
 export default function EvangelisationHub() {
   const router = useRouter();
