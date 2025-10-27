9318dd4a54a1126ef6ec2a4c70ac3d5405af6926
allanbavajee <allan.bavajee@gmail.com>
2025-10-27 14:17:52 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,5 +16,13 @@ export default function IndexPage() {
       <h1 className="text-3xl font-bold mb-4">🏠 Page d'accueil</h1>
       <p className="text-lg mb-6">Bienvenue {userEmail}</p>
     </div>
+  <div
+          onClick={() => handleRedirect("/membres-hub")}
+          className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-blue-500 p-3 hover:shadow-lg transition-all duration-200 cursor-pointer"
+        >
+          <div className="text-4xl mb-1">👤</div>
+          <div className="text-lg font-bold text-gray-800">Suivis des membres</div>
+        </div>
+  
   );
 }
