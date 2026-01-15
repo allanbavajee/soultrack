f0f3457583040781fa481ab6b42d52ea72e211a1
allanbavajee <allan.bavajee@gmail.com>
2026-01-15 09:10:40 +0400

    Update evangelisation-hub.js

--- pages/evangelisation-hub.js
+++ pages/evangelisation-hub.js
@@ -63,17 +63,7 @@ export default function EvangelisationHub() {
       </div>
 
       {/* 🔹 Cartes principales */}
-      <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6">
-        {/* Ajouter un évangélisé */}
-        <Link
-          href="/add-evangelise"
-          className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#4285F4] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
-        >
-          <div className="text-4xl mb-2">➕</div>
-          <div className="text-lg font-bold text-gray-800 text-center">
-            Ajouter un évangélisé
-          </div>
-        </Link>
+      <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6">     
 
         {/* Liste des évangélisés */}
         <Link
@@ -96,6 +86,18 @@ export default function EvangelisationHub() {
             Suivis des évangélisés
           </div>
         </Link>
+
+          {/* Suivis des évangélisés */}
+        <Link
+          href="/Rapport-evangelisation"
+          className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#ff9800] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+        >
+          <div className="text-4xl mb-2">🌱</div>
+          <div className="text-lg font-bold text-gray-800 text-center">
+            Rapport Evangélisation
+          </div>
+        </Link>
+            
       </div>
 
       {/* 🔹 Bouton popup ajouté sous les cartes */}
