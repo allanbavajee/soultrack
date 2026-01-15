9c80011a4a885beaef19f7e6437e71d3bb33c0b1
allanbavajee <allan.bavajee@gmail.com>
2026-01-15 10:16:27 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+// _app.js
 import "../styles/globals.css";
+import { MembersProvider } from "../context/MembersContext"; // ⚠️ Assure-toi que le chemin est correct
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MembersProvider>
+      <Component {...pageProps} />
+    </MembersProvider>
+  );
 }
 
 export default MyApp;
