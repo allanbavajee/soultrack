633f23b9332ac90a39c218fe17f4c8d9f85414f0
allanbavajee <allan.bavajee@gmail.com>
2026-01-15 09:12:35 +0400

    Update membres-hub.js

--- pages/membres-hub.js
+++ pages/membres-hub.js
@@ -6,6 +6,7 @@ import LogoutLink from "../components/LogoutLink";
 import SendLinkPopup from "../components/SendLinkPopup";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import HeaderPages from "../components/HeaderPages";
 
 export default function MembresHub() {
   const router = useRouter();
@@ -22,30 +23,7 @@ export default function MembresHub() {
       className="min-h-screen flex flex-col items-center p-6 text-center space-y-6"
       style={{ background: "linear-gradient(135deg, #2E3192 0%, #92EFFD 100%)" }}
     >
-      {/* 🔹 Top bar */}
-      <div className="w-full max-w-5xl mb-6">
-        <div className="flex justify-between items-center">
-          <button
-            onClick={() => router.back()}
-            className="flex items-center text-white hover:text-gray-200 transition-colors"
-          >
-            ← Retour
-          </button>
-
-          <LogoutLink />
-        </div>
-
-        <div className="flex justify-end mt-2">
-          <p className="text-orange-200 text-sm">
-            👋 Bienvenue {userName}
-          </p>
-        </div>
-      </div>
-
-      {/* 🔹 Logo centré */}
-      <div className="mb-6">
-        <Image src="/logo.png" alt="SoulTrack Logo" className="w-20 h-18 mx-auto" />
-      </div>
+      <HeaderPages />
 
       {/* 🔹 Titre + texte motivant */}
       <div className="text-center mb-6">
@@ -79,6 +57,15 @@ export default function MembresHub() {
             <div className="text-lg font-bold text-gray-800 text-center">Liste des Conseillers</div>
           </Link>
         </div>
+
+          <Link
+          href="/attendance"
+          className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
+          border-t-4 border-[#0D9488] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+        >
+          <div className="text-4xl mb-2">⛪</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Rapport du Culte</div>
+        </Link>
         
         {/* 🔹 Bouton popup ajouté sous les cartes */}
         <div className="w-full max-w-md mb-10">
