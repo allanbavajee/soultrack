81e033bcb682747acb609b014829a422d6a104e5
allanbavajee <allan.bavajee@gmail.com>
2026-04-19 18:24:22 +0400

    Update contact.js

--- pages/site/contact.js
+++ pages/site/contact.js
@@ -24,10 +24,10 @@ export default function ContactPage() {
 
   const navItems = [
     { label: "Accueil", path: "/site/HomePage" },
-    { label: "Process", path: "/CommentCaMarche" },
-    { label: "À propos", path: "/about" },
-    { label: "Pricing", path: "/pricing" },
-    { label: "Contact", path: "/contact" },
+    { label: "Fonctionnement", path: "site/Fonctionnement" },
+    { label: "À propos", path: "site/about" },
+    { label: "Pricing", path: "site/pricing" },
+    { label: "Contact", path: "site/contact" },
   ];
 
   const handleSubmit = () => {
